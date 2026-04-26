bda7fe3d788e85ea7dd7b7e61bc4c5727623e616
krajarajan96 <kraju1996@gmail.com>
2026-04-26 15:54:49 +0530

    feat: Version code 4

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -43,7 +43,7 @@ android {
         applicationId = "com.mafiasd.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.0"
     }
 
